4e2db0e0b91204f1ca4da809b9cb21d5a47cde95
Vincent Wang <a36168120@gmail.com>
2019-04-18 16:37:43 -0700

    slider progess

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // All of my code goes in here //
+    console.log('this is working');
+
+
+    const flkty = new Flickity('.main-carousel', {
+        cellAlign: 'left',
+        wrapAround: true,
+        freeScroll: true
+    });
+
+
+
+});  // End of my code//
+
