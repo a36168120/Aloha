9cf42889c8acdb9e7cf70f9191a03191f4eeb001
Vincent Wang <a36168120@gmail.com>
2019-04-22 21:36:22 -0700

    Final

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     // All of my code goes in here //
-    console.log('this is working');
+
 
 
     const flkty = new Flickity('.main-carousel', {
         cellAlign: 'left',
         wrapAround: true,
         freeScroll: true
+
     });
 
 
