f648d963b8459616e038e754637e9a8e063f1670
Vincent Wang <a36168120@gmail.com>
2019-04-23 07:06:38 -0700

    Complete

--- js/main.js
+++ js/main.js
@@ -2,15 +2,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // All of my code goes in here //
 
-
-
     const flkty = new Flickity('.main-carousel', {
         cellAlign: 'left',
         wrapAround: true,
         freeScroll: true
-
     });
 
+    const form = document.getElementById("form");
+    const email = document.getElementById("email");
+
+    form.addEventListener("submit", function (popUp) {
+        popUp.preventDefault();
+        if (email.value != "") {
+            alert("Thank you for subscribing!");
+            form.submit();
+        }
+        else {
+            alert("Please enter a valid email")
+        }
+    });
 
 
 });  // End of my code//
